f845f00e686170a07cc4f40bd063dd1d1eb1e772
Harry <hjwp2@cantab.net>
2017-01-29 14:24:17 +0000

    A first attempt at a decorator to do a wait on any fn. --ch18l006--

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -7,6 +7,19 @@ import time
 MAX_WAIT = 5
 
 
+def wait(fn):
+    def modified_fn():
+        start_time = time.time()
+        while True:
+            try:
+                return fn()
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.1)
+    return modified_fn
+
+
 
 class FunctionalTest(StaticLiveServerTestCase):
 
